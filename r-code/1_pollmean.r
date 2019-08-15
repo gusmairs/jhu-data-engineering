cfbfcf29cc38d96a47236d670ddb10f3cdac1172
gusmairs <gusmairs@mac.com>
2019-08-15 10:55:51 -0700

    new approach to data file path, text file holds it

--- r-code/1_pollmean.r
+++ r-code/1_pollmean.r
@@ -1,12 +1,13 @@
-#!/usr/bin/env Rscript
+#!/usr/bin/env rscript
 
-# 1
+# Part 1
 # Reads a range of data from a large batch of csv files and calculates a mean
 # The file.path() function creates a correct path based on the platform
 # running R. Note rbind() ignores identical headers and is very very slow
 # here due to memory management. Worth exploring making R work like Python
 # in building a list of dataframes and then concatenating them.
 
+# Production use:
 # In R, source this file, set a 'dir' variable to the dataset path
 # and call the pollutant_mean function with parameters. Current version
 # unzips the data file, runs the script, then deletes the unzipped directory.
--- r-code/2_complete.r
+++ r-code/2_complete.r
@@ -1,15 +1,23 @@
 #!/usr/bin/env rscript
 
-# 2
+# Part 2
 # Reads data from files as above and compiles a table of complete cases
 
-get_complete <- function(directory, id = 1:332) {
-    obs_table <- data.frame("id" = numeric(), "nobs" = numeric())
+get_complete <- function(id = 1:332) {
+    con <- file('r-code/data_path.txt', open = 'r')
+    dir <- readLines(con)
+    close(con)
+    if (!dir.exists(file.path(dir, 'specdata'))) {
+        unzip(file.path(dir, 'specdata.zip'), exdir = dir)
+    }
+    obs_table <- data.frame('id' = numeric(), 'nobs' = numeric())
     for (x in id) {
-        setx <- read.csv(
-            file.path(directory, paste0(sprintf("%03d", x), ".csv")))
-        obs_table[nrow(obs_table) + 1, ] <- c(x, sum(complete.cases(setx)))
+        set_x <- read.csv(
+            file.path(
+                dir, 'specdata', paste0(sprintf('%03d', x), '.csv')
+            )
+        )
+        obs_table[nrow(obs_table) + 1, ] <- c(x, sum(complete.cases(set_x)))
     }
     obs_table
 }
-print(get_complete('specdata'))
