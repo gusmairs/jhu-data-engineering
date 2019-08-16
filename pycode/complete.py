1eee9d4b622631544da50015ef148a50f7f5b106
gusmairs <gusmairs@mac.com>
2019-08-15 17:00:03 -0700

    python working, needs refactoring and polish

--- pycode/complete.py
+++ pycode/complete.py
@@ -7,7 +7,7 @@ import pandas as pd
 from os import path
 
 def obs(dat_list=range(1, 333)):
-    with open('py-code/data_path.txt', 'r') as f:
+    with open('pycode/data_path.txt', 'r') as f:
         dir = f.readlines()[0].replace('\n', '')
     obs_list = []
     for dat in dat_list:
@@ -15,5 +15,3 @@ def obs(dat_list=range(1, 333)):
         dat_df = pd.read_csv(p)
         obs_list.append([dat, len(dat_df.dropna())])
     return pd.DataFrame(obs_list, columns=('id', 'nobs'))
-
-print(obs(range(1, 10)))
--- pycode/corr.py
+++ pycode/corr.py
@@ -2,21 +2,17 @@
 
 import pandas as pd
 from os import path
+from pycode.complete import obs
 
-def obs(dir, dat_list=range(1, 333)):
-    obs_list = []
-    for dat in dat_list:
-        p = path.join(dir, '{0:03d}'.format(dat) + '.csv')
-        dat_df = pd.read_csv(p)
-        obs_list.append([dat, len(dat_df.dropna())])
-    return pd.DataFrame(obs_list, columns=('id', 'nobs'))
-
-def corr(dir, thresh):
-    valid_obs = obs(dir)[obs(dir).nobs > thresh].id
+def corr(thresh):
+    with open('pycode/data_path.txt', 'r') as f:
+        dir = f.readlines()[0].replace('\n', '')
+    all_dat = obs()
+    valid_obs = all_dat.loc[all_dat.nobs > thresh].id
     corr_list = []
     for id in valid_obs:
-        dat = pd.read_csv(path.join(dir, '{0:03d}'.format(id) + '.csv'))
+        dat = pd.read_csv(
+            path.join(dir, 'specdata', '{0:03d}'.format(id) + '.csv')
+        )
         corr_list.append([dat['sulfate'].corr(dat['nitrate'])])
     return pd.DataFrame(corr_list)
-
-print(corr('specdata', 1000))]
